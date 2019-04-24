19d86c3136474476aa07aa13fecc4aa2303d30de
Al1616 <rm3495@columbia.edu>
2019-04-24 19:42:22 -0400

    murder map done

--- map.js
+++ map.js
@@ -1,1 +1,91 @@
-map.js
+console.log("Hello, world!")
+// your mapbox token
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWwxNjE2IiwiYSI6ImNqc200ZXQ0YTBnOWE0NG54Ym45YnYybHgifQ.t9lM7oOjsxtKmQS_BGfbdg'
+
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/al1616/cjuvre6br4e2r1gpj0egk3tga',
+    center: [-66.08392,18.41259], 
+    zoom: 12
+})
+
+// create an instance of NavigationControl
+let navigation = new mapboxgl.NavigationControl({
+    showCompass: false
+})
+
+// add the navigation to your map
+map.addControl(navigation, 'top-left')
+
+// create an instance of ScaleControl
+let scale = new mapboxgl.ScaleControl({
+    maxWidth: 80,
+    unit: 'imperial'
+})
+
+// add the scale to your map
+map.addControl(scale, 'bottom-right')
+
+let geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true,
+    showUserLocation: true,
+    fitBoundsOptions: {
+    }
+})
+
+map.addControl(geolocate, 'top-left')
+
+geolocate.on('geolocate', function(event) {
+
+    // create new variables to store the attributes we're interested in from the event
+    let lng = event.coords.longitude
+    let lat = event.coords.latitude
+
+    // debug
+    console.log('geolocated:', lng, lat)
+
+    // format lng lat values and display them on our 'info' element
+    document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)
+
+})
+
+map.on('click', function(event) {
+
+    let lng = event.lngLat.lng
+    let lat = event.lngLat.lat
+
+    console.log("clicked:", lng, lat)
+
+    document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)
+
+})
+
+let data = [
+    {
+        location: [-73.96191,40.80762],
+        content: 'I like to eat my lunch here<br /><img src="https://currystonefoundation.org/wp-content/uploads/2018/05/csf_pr_csr_image5.jpg" />'
+    },
+    {
+        location: [-73.95936,40.80610],
+        content: '15 years ago, you could see over the trees'
+    },
+    {
+        location: [-73.96204,40.80994],
+        content: 'This was once tennis courts'
+    },
+    ]
+
+    data.forEach(function(d) {
+
+    let marker = new mapboxgl.Marker()    
+    marker.setLngLat(d.location)
+    marker.addTo(map)  
+
+    let popup = new mapboxgl.Popup()
+    popup.setHTML(d.content)
+    marker.setPopup(popup)
+
+})
